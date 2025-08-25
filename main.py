58423865def65773453ea73425801e111110a702
Luis Felipe Teobaldo <luis.teobaldo09@aluno.ifce.edu.br>
2025-08-25 13:47:12 +0000

    parte2

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = FastAPI(title="API da Suzane")
 @app.get("/")
 def helloWorld():
     return {"message": "Hello World!"}
+
+@app.get("/aluno")
+def get_aluno():
+    return{"insira na rota após a '/' o nome do aluno"}
+
+@app.get("/aluno/{nome_aluno}")
+def get_aluno_by_name(nome_aluno: str):
+    return{"o nome do aluno é": nome_aluno}
